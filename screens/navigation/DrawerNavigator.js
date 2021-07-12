938d0888b5769b3f5bdb94eb35bf4673a78a7626
pavanimallela <pavani.mallela@gmail.com>
2021-07-12 11:25:58 +0530

    changes

---
+++ screens/navigation/DrawerNavigator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import TabNavigator from './TabNavigator';
+import Profile from '../Profile';
+
+const Drawer = createDrawerNavigator();
+const DrawerNavigator =()=>{
+    return(
+        <DrawerNavigator>
+            <Drawer.Screen name ="Home" component={TabNavigator} />
+            <Drawer.Screen name ="Profile" component={Profile} />
+        </DrawerNavigator>
+    )
+}
+
+export default DrawerNavigator;
